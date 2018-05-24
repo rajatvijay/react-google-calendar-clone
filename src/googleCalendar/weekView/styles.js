eaacf41b443cf461ae0863c0d820a8f28b3e3740
rajatvijay <rajatvijay5@gmail.com>
2018-05-25 01:03:49 +0530

    added missing stylesfiles

---
+++ src/googleCalendar/weekView/styles.js
@@ -0,0 +1,101 @@
+export const col = {
+  border: '#e0e0e0 1px solid',
+};
+export const weekDays = {
+  height: 100,
+};
+
+export const weekDayName = {
+  fontSize: 12,
+  lineHeight: '32px',
+  textTransform: 'capitalize',
+  color: '#757575',
+  marginLeft: 10,
+};
+
+export const container = {
+  paddingRight: '10em',
+  paddingTop: '2em',
+  paddingBottom: '2em',
+};
+
+export const weekDates = {
+  fontSize: 40,
+  lineHeight: '12px',
+  color: '#9e9e9e',
+  marginLeft: 10,
+};
+
+export const slot = {
+  height: 40,
+  cursor: 'pointer',
+};
+export const timeCol = {
+  fontSize: 10,
+  color: '#212121',
+  textAlign: 'right',
+  padding: 6,
+};
+
+export const timeString = {
+  position: 'absolute',
+  right: 8,
+  top: -8,
+};
+
+export const row = {
+  position: 'relative',
+};
+
+export const toolbar = {
+  paddingBottom: 10,
+};
+
+export const toolbarDate = {
+  fontSize: 20,
+  fontWeight: 400,
+  opacity: 0.54,
+  lineHeight: '30px',
+  textAlign: 'right',
+};
+
+export const appTitle = {
+  fontSize: 20,
+  fontWeight: 400,
+  lineHeight: '30px',
+};
+
+export const alignRight = {
+  textAlign: 'right',
+};
+
+export const spacify = {
+  marginRight: 5,
+};
+
+export const inputStyles = {
+  marginTop: 25,
+  marginBottom: 15,
+};
+
+export const weekButtons = {
+  paddingRight: 0,
+  paddingLeft: 14,
+  textAlign: 'center',
+};
+
+export const lightHighlighter = {
+  backgroundColor: 'rgba(0,0,0,0.04)',
+};
+
+export const eventHighlighter = {
+  position: 'absolute',
+  backgroundColor: '#b3e1f7',
+  border: '1px solid #81cdf2',
+  borderRadius: '4px',
+  color: 'white',
+  padding: '2px 4px',
+  fontSize: '12px',
+  zIndex: 1,
+  cursor: 'pointer',
+};
